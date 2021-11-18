69f155a2e22dea9d8c69018d5aba626b63f88f6d
khagen Baro <74665369+khagenbaro@users.noreply.github.com>
2021-11-18 23:55:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ function calculateTip() {
     var numOfPeople = document.getElementById("peopleamt").value;
   
     //validate input
-    if (billAmt === "" || serviceQual == 0) {
-      alert("Please enter values");
+    if (billAmt == '' || serviceQual == 0 || billAmt <= 0) {
+      alert("Please enter bill Amount in positive integers");
       return;
     }
     //Check to see if this input is empty or less than or equal to 1
     if (numOfPeople === "" || numOfPeople <= 1) {
+      alert("Please enter no of people in positive integers");
       numOfPeople = 1;
       document.getElementById("each").style.display = "none";
     } else {
